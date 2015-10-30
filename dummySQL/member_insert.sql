1b08b0acc4c712011292bd11acd419903e9a26b7
miho Oki <miho-ooki@tg-inet.co.jp>
2015-10-30 14:03:16 +0900

    character edit

--- dummySQL/member_insert.sql
+++ dummySQL/member_insert.sql
@@ -1,7 +1,7 @@
-INSERT INTO MEMBER VALUES('aa', 'ëΩñÿ', '2', 0);
-INSERT INTO MEMBER VALUES('bb', 'çïìc', '3', 0);
-INSERT INTO MEMBER VALUES('vv', 'àÓìc', '1', 0);
-INSERT INTO MEMBER VALUES('dd', 'è¨ó—', '1', 0);
-INSERT INTO MEMBER VALUES('ee', 'çrñÏ', '1', 0);
-INSERT INTO MEMBER VALUES('ff', 'ñqìá', '1', 0);
-INSERT INTO MEMBER VALUES('gg', 'ìnïî', '1', 0);
+INSERT INTO MEMBER VALUES('aa', 'Â§öÊú®', '2', 0);
+INSERT INTO MEMBER VALUES('bb', 'ÈªíÁî∞', '3', 0);
+INSERT INTO MEMBER VALUES('vv', 'Á®≤Áî∞', '1', 0);
+INSERT INTO MEMBER VALUES('dd', 'Â∞èÊûó', '1', 0);
+INSERT INTO MEMBER VALUES('ee', 'ËçíÈáé', '1', 0);
+INSERT INTO MEMBER VALUES('ff', 'ÁâßÂ≥∂', '1', 0);
+INSERT INTO MEMBER VALUES('gg', 'Ê∏°ÈÉ®', '1', 0);
