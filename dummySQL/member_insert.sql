01f0063ec3512fc82b5fc12b89c2cded609311ed
oki mih <torino.torinist@gmail.com>
2015-11-07 14:22:12 +0900

    sql edit

--- dummySQL/member_insert.sql
+++ dummySQL/member_insert.sql
@@ -1,7 +1,7 @@
-INSERT INTO MEMBER VALUES('aa', '多木', '2', 0);
-INSERT INTO MEMBER VALUES('bb', '黒田', '3', 0);
-INSERT INTO MEMBER VALUES('vv', '稲田', '1', 0);
-INSERT INTO MEMBER VALUES('dd', '小林', '1', 0);
-INSERT INTO MEMBER VALUES('ee', '荒野', '1', 0);
-INSERT INTO MEMBER VALUES('ff', '牧島', '1', 0);
-INSERT INTO MEMBER VALUES('gg', '渡部', '1', 0);
+INSERT INTO appsahdb.MEMBER VALUES('aa', '多木', '2', 0);
+INSERT INTO appsahdb.MEMBER VALUES('bb', '黒田', '3', 0);
+INSERT INTO appsahdb.MEMBER VALUES('vv', '稲田', '1', 0);
+INSERT INTO appsahdb.MEMBER VALUES('dd', '小林', '1', 0);
+INSERT INTO appsahdb.MEMBER VALUES('ee', '荒野', '1', 0);
+INSERT INTO appsahdb.MEMBER VALUES('ff', '牧島', '1', 0);
+INSERT INTO appsahdb.MEMBER VALUES('gg', '渡部', '1', 0);
